0c9912c4060a5a919da14c7fba36a834d118df14
Highlander M <highlander9200@gmail.com>
2022-11-18 10:03:45 -0300

    Show tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import './App.css'
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import './index.css'
 
 function App() {
-
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:20pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+  ])
 
   return (
-    <div className="App">
-      
+    <div className='container'>
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
