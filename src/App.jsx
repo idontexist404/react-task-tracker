91135fec2ad723b105f82b03f89fc5e9a8f7987a
Highlander M <highlander9200@gmail.com>
2022-11-22 10:07:59 -0300

    Show close tasks

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Tasks from './components/Tasks'
 import './index.css'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -27,7 +28,6 @@ function App() {
   ])
 
   // Add task
-
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task }
@@ -50,8 +50,11 @@ function App() {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
